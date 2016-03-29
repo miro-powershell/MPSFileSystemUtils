7d319f61910b1372926444e84629c541edaff508
Miro H <miro-powershell@users.noreply.github.com>
2016-03-29 23:29:22 +0200

    Added files via upload

---
+++ PlayWithFileSystemWatcher.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module -Name MPSFileSystemUtils
+
+New-Item -Path C:\Watcher -ItemType Directory -Force | Out-Null
+
+$jobs = Start-FileSystemWatcher -Path C:\Watcher -Filter "*.txt"
+1..10 | ForEach-Object {New-Item -Path "C:\Watcher\new$_.txt" -ItemType File -ErrorAction SilentlyContinue} | Out-Null
+1..10 | ForEach-Object {Add-Content -Path "C:\Watcher\new$_.txt" -Value "Just something" -ErrorAction SilentlyContinue}  | Out-Null
+2..5 | ForEach-Object {Remove-Item -Path "C:\Watcher\new$_.txt" -ErrorAction SilentlyContinue} | Out-Null
+1..10 | ForEach-Object {Rename-Item -Path "C:\Watcher\new$_.txt" -NewName "C:\Watcher\new$($_ * 10).txt" -ErrorAction SilentlyContinue} | Out-Null
+Get-ChildItem C:\Watcher | Remove-Item -Force -ErrorAction SilentlyContinue | Out-Null
+$Result = Stop-FileSystemWatcher
+
+
+$Result | Sort-Object -Property Time
+
+Remove-Item -Path C:\Watcher -Recurse -Force -ErrorAction SilentlyContinue | Out-Null 
